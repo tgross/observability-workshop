fbabfd7788cf71e130526d5b2eff88293dc030a4
Tim Gross <tim@0x74696d.com>
2016-10-17 16:28:17 -0400

    Add interfaces spec to consul agent too

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,9 +4,13 @@ image="ubuntu-certified-16.04"
 package="k4-highcpu-kvm-1.75G"
 
 create() {
-    count=$((${1:-1} + 0))
+    local count=$((${1:-1} + 0))
+    local private=$(triton network ls -l | awk -F' +' '/My-Fabric-Network/{print $1}')
+    local public=$(triton network ls -l | awk -F' +' '/Joyent-SDC-Public/{print $1}')
     for (( c=1; c<=${count}; c++ )); do
-        triton instance create --name="student-${c}" "${image}" "${package}" \
+        triton instance create \
+               --name="student-${c}" "${image}" "${package}" \
+               --network="${private},${public}" \
                --script=./userscript.sh
     done
 }
@@ -18,6 +22,16 @@ inventory() {
 
 provision() {
     ansible-playbook -v -i ./inventory vm.yml
+}
+
+supporting() {
+    export COMPOSE_PROJECT_NAME=workshop
+    export TRITON_ACCOUNT=$(triton account get | awk -F": " '/id:/{print $2}')
+    pushd $(pwd)
+    cd supporting
+    docker-compose up -d
+    docker-compose scale consul=3
+    popd
 }
 
 cmd=$1
