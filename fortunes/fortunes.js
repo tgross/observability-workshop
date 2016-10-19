766328a861dc7b48c47fa903fc9bc8293cd2aaba
Tim Gross <tim@0x74696d.com>
2016-10-19 17:07:53 -0400

    clean up fortunes.js callbacks

--- fortunes/fortunes.js
+++ fortunes/fortunes.js
@@ -3,34 +3,31 @@
 const Http = require('http');
 const Data = require('./lib/data');
 
-// ------------------------------------------------
-
-
 // The root route queries MySQL and fills in a template
 // with the data
 const getRoot = function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  return res.end(body);
+  Data.select((err, body) => {
+    if (err) {
+      res.writeHead(500);
+      return res.end(err);
+    }
+    res.writeHead(200);
+    return res.end(body);
+  });
 }
-
-
-// ------------------------------------------------
 
 // The /health route is used only by the health check to
 // verify we're serving content
 const getHealth = function (req, res) {
-  body = { 'healthy': true };
   Data.check((err) => {
     if (err) {
       res.writeHead(500);
-      return
+      return res.end(err);
     }
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    return res.end(JSON.stringify(body));
+    res.writeHead(200);
+    return res.end("ok");
   });
 }
-
-// ------------------------------------------------
 
 const server = Http.createServer((req, res) => {
   if (req.path == '/health') {
@@ -38,7 +35,6 @@ const server = Http.createServer((req, res) => {
   }
   return getRoot(req, res);
 });
-
 
 server.listen(3000, () => {
   console.log('Running Fortunes on port 3000');
