8f8dd128e675a68dfd09467a41f54c4b0bed78f1
Tim Gross <tim@0x74696d.com>
2016-10-20 14:39:43 -0400

    Set request ids in all requests, add simple logging to fortunes

--- fortunes/fortunes.js
+++ fortunes/fortunes.js
@@ -3,6 +3,20 @@
 const Http = require('http');
 const Data = require('./lib/data');
 const Template = require('./lib/template');
+const util = require('util');
+
+
+const log = function(req, code, msg) {
+  var now = new Date().toISOString();
+  var reqId = req.headers["x-request-id"];
+  if (!reqId) {
+    reqId = "-";
+  }
+  if (!msg) {
+    msg = '-'
+  }
+  console.log('%s %s %s %s', now, reqId, req.url, msg)
+}
 
 // The root route queries MySQL and fills in a template
 // with the data
@@ -10,42 +24,46 @@ const getRoot = function (req, res) {
   Data.select((err, content) => {
     if (err) {
       res.writeHead(500);
-      console.error(err);
+      log(req, 500, err);
       return
     }
     Template.render(content, (err, body) => {
       if (err) {
         res.writeHead(500);
-        console.error(err);
+        log(req, 500, err);
         return
       }
       res.writeHead(200);
+      var now = new Date().toISOString()
+      log(req, 200);
       return res.end(body);
     });
   });
 }
 
 // The /health route is used only by the health check to
-// verify we're serving content
+// verify we can connect to the DB and can serve content
 const getHealth = function (req, res) {
   Data.check((err) => {
     if (err) {
       res.writeHead(500);
-      console.error(err);
+      log(req, 500, err);
       return
     }
     res.writeHead(200);
+    log(req, 200);
     return res.end("ok");
   });
 }
 
 const server = Http.createServer((req, res) => {
-  if (req.path == '/health') {
+  if (req.url == '/health') {
     return getHealth(req, res);
   }
   return getRoot(req, res);
 });
 
 server.listen(3000, () => {
-  console.log('Running Fortunes on port 3000');
+  var now = new Date().toISOString();
+  console.log('[%s] Running Fortunes on port 3000', now);
 });
